f23e7f01fbe77c431d04331b478f3d4fbe66ba58
Prakash Maity <105304137+PrakashMaity@users.noreply.github.com>
2023-02-18 12:38:53 +0530

    first commit

---
+++ components/molecules/signupButton/SignupButton.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignupButton = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  )
+}
+
+export default SignupButton
+
+const styles = StyleSheet.create({})
---
+++ components/organisms/signupSection/signupSection.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignupSection = () => {
+  return (
+    <View>
+      <Text>SignupSection</Text>
+    </View>
+  )
+}
+
+export default SignupSection
+
+const styles = StyleSheet.create({})
