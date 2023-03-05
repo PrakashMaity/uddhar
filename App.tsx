3bbb6d647e07cf1a72e618fb592e9305090d2467
Prakash Maity <105304137+PrakashMaity@users.noreply.github.com>
2023-03-06 01:25:34 +0530

    react realm added

--- App.tsx
+++ App.tsx
@@ -5,12 +5,40 @@ import { NativeBaseProvider } from 'native-base';
 import { styleConst } from './src/style/root.style';
 import StackNavigation from './src/navigation/stackNavigator/StackNavigation';
 import './assets/i18n/i18n.config'
+import { Realm, createRealmContext } from '@realm/react'
+class Task extends Realm.Object {
+  _id!: Realm.BSON.ObjectId;
+  description!: string;
+  isComplete!: boolean;
+  createdAt!: Date;
+
+  static generate(description: string) {
+    return {
+      _id: new Realm.BSON.ObjectId(),
+      description,
+      createdAt: new Date(),
+    };
+  }
+
+  static schema = {
+    name: 'Task',
+    primaryKey: '_id',
+    properties: {
+      _id: 'objectId',
+      description: 'string',
+      isComplete: { type: 'bool', default: false },
+      createdAt: 'date'
+    },
+  };
+}
+
+const { RealmProvider, useRealm, useQuery } = createRealmContext({ schema: [Task] })
+
 const App = () => {
   return (
-    // <NativeBaseProvider>
-    // <StatusBar barStyle={"light-content"} backgroundColor={styleConst.COLOR.VIOLET.VIOLET_100} />
-    <StackNavigation />
-    // <NativeBaseProvider />
+    <RealmProvider>
+     <StackNavigation />
+    </RealmProvider>
   )
 };
 export default App;
