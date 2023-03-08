bdb5c42a052c54132ee06fc011f184bb4a5320da
Prakash Maity <105304137+PrakashMaity@users.noreply.github.com>
2023-03-08 10:15:40 +0530

    style added

--- src/pages/auth/Intro/IntroScreen/IntroScreen.page.tsx
+++ src/pages/auth/Intro/IntroScreen/IntroScreen.page.tsx
@@ -7,6 +7,7 @@ import { styleConst } from "../../../../style/root.style";
 const { width } = Dimensions.get("window");
 import { useTranslation } from "react-i18next";
 import { sliderData } from "./pageData/SliderData";
+import { styles } from "./IntroScreen.style";
 
 export default function IntroScreen() {
   const navigation:any = useNavigation();
@@ -21,7 +22,7 @@ export default function IntroScreen() {
   };
 
   return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
+    <View style={styles.mainContainer}>
       <View style={{ flex: 1 }}>
         <ScrollView
           onMomentumScrollEnd={handleScrollEnd}
--- src/pages/auth/Intro/SplashScreen/SplashScreen.page.tsx
+++ src/pages/auth/Intro/SplashScreen/SplashScreen.page.tsx
@@ -1,10 +1,9 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
-import { styleConst } from '../../../../style/root.style'
-import { responsiveHeight, responsiveWidth } from '../../../../utils/ResponsiveUI'
 import { Image } from 'react-native'
 import { images } from '../../../../constant/images.contant'
 import { useNavigation } from '@react-navigation/native'
+import { styles } from './SplashScreen.style'
 export default function SplashScreen() {
   const navigation:any = useNavigation()
   const greet = () => {
@@ -15,11 +14,11 @@ export default function SplashScreen() {
   }, [])
 
   return (
-    <View style={{ flex: 1, backgroundColor: styleConst.COLOR.VIOLET.VIOLET_100 }}>
-      <View style={{ height: responsiveHeight(85), width: responsiveHeight(85), position: "absolute", top: responsiveHeight(374), left: responsiveWidth(118) }} >
-        <Image source={images.SplashScreen.dot} style={{ height: "100%", width: "100%", resizeMode: "cover" }} />
+    <View style={styles.mainContainer}>
+      <View style={styles.imageWrapper} >
+        <Image source={images.SplashScreen.dot} style={styles.mainImages} />
       </View>
-      <Text style={{ marginTop: responsiveHeight(372), marginLeft: responsiveWidth(89), fontSize: responsiveHeight(53), fontFamily: styleConst.FONT_FAMILY.INTER_BOLD, color: styleConst.COLOR.LIGHT.LIGHT_100, lineHeight: responsiveHeight(67.77) }} >uddhar</Text>
+      <Text style={styles.mainTitle} >uddhar</Text>
     </View>
   )
 }
